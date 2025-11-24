cf500e1efa76076f3e554c3a0c85692797b530e2
Alex Karasev <alex333karasev@yandex.ru>
2025-11-24 04:06:56 +0300

    4.2 final

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -9,8 +9,7 @@ def test_guest_can_go_to_login_page(browser):
     main_page = MainPage(browser, url)
     main_page.open()
     main_page.go_to_login_page()
-    login_url = 'https://selenium1py.pythonanywhere.com/ru/accounts/login/'
-    login_page = LoginPage(browser, login_url)
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
 
